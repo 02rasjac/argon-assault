c874a9bf44cde156bb50804638de9ca406d8cbfa
02rasjac <rasmusjacklin2002@gmail.com>
2021-05-03 19:35:19 +0200

    Lecture #77: Add constrain on movement -> prevent going of screen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,8 +3,10 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] InputAction movement;
+    [Tooltip("X respectivly Y are symmetrical")]
+    [SerializeField] Vector2 clampMinMax;
     [SerializeField] float speed = 10f;
+    [SerializeField] InputAction movement;
 
     void OnEnable() {
         movement.Enable();
@@ -19,11 +21,19 @@ public class PlayerController : MonoBehaviour
         float horiz = movement.ReadValue<Vector2>().x;
         float verti = movement.ReadValue<Vector2>().y;
 
-        // Move the player by setting the local position
-        transform.localPosition = new Vector3(
-            transform.localPosition.x + (speed * horiz * Time.deltaTime),
-            transform.localPosition.y + (speed * verti * Time.deltaTime),
-            transform.localPosition.z
+        // Clamp x- and y-position to prevent player moving out of screen
+        float newX = Mathf.Clamp(
+            transform.localPosition.x + (speed * horiz * Time.deltaTime), 
+            -clampMinMax.x, 
+            clampMinMax.x
         );
+        float newY = Mathf.Clamp(
+            transform.localPosition.y + (speed * verti * Time.deltaTime), 
+            -clampMinMax.y, 
+            clampMinMax.y
+        );
+
+        // Move the player by setting the local position
+        transform.localPosition = new Vector3(newX, newY, transform.localPosition.z);
     }
 }
