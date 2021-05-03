a8529be18e3b71081d1a2e55da628362845a99fc
02rasjac <rasmusjacklin2002@gmail.com>
2021-05-03 20:22:27 +0200

    Lecture #79: Rotate player for more dynamic feeling

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,15 @@ public class PlayerController : MonoBehaviour
     [Tooltip("X respectivly Y are symmetrical")]
     [SerializeField] Vector2 clampMinMax;
     [SerializeField] float speed = 10f;
+
+    [SerializeField] float pitchPositionFactor = 3f;
+    [SerializeField] float pitchMovingFactor = 3f;
+    [SerializeField] float yawPositionFactor = 2f;
+    [SerializeField] float rollMovingFactor = 2f;
+
     [SerializeField] InputAction movement;
+
+    float horiz, verti;
 
     void OnEnable() {
         movement.Enable();
@@ -18,18 +26,35 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        float horiz = movement.ReadValue<Vector2>().x;
-        float verti = movement.ReadValue<Vector2>().y;
+        horiz = movement.ReadValue<Vector2>().x;
+        verti = movement.ReadValue<Vector2>().y;
+        
+        translate();
+        rotate();
+    }
 
+    void rotate() {
+        // Pitch when moving, and based on position
+        float pitch = transform.localPosition.y * -pitchPositionFactor - pitchMovingFactor * verti;
+        // Yaw based on horizontal position
+        float yaw = transform.localPosition.x * -yawPositionFactor;
+        // Roll when moving horizontally
+        float roll = -rollMovingFactor * horiz;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    void translate()
+    {
         // Clamp x- and y-position to prevent player moving out of screen
         float newX = Mathf.Clamp(
-            transform.localPosition.x + (speed * horiz * Time.deltaTime), 
-            -clampMinMax.x, 
+            transform.localPosition.x + (speed * horiz * Time.deltaTime),
+            -clampMinMax.x,
             clampMinMax.x
         );
         float newY = Mathf.Clamp(
-            transform.localPosition.y + (speed * verti * Time.deltaTime), 
-            -clampMinMax.y, 
+            transform.localPosition.y + (speed * verti * Time.deltaTime),
+            -clampMinMax.y,
             clampMinMax.y
         );
 
