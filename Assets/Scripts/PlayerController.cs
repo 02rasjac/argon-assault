c95557ef7be5f492736ffa3bc48f0c9731aeaa54
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2022-07-02 14:21:03 +0200

    Lecture #81: Lengthen rail and do some tuning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,17 @@ public class PlayerController : MonoBehaviour
 {
     [Tooltip("X respectivly Y are symmetrical")]
     [SerializeField] Vector2 clampMinMax;
-    [SerializeField] float speed = 10f;
+    [SerializeField] float speed = 30f;
 
-    [SerializeField] float pitchPositionFactor = 3f;
-    [SerializeField] float pitchMovingFactor = 3f;
-    [SerializeField] float yawPositionFactor = 2f;
-    [SerializeField] float rollMovingFactor = 2f;
+    [SerializeField] float pitchPositionFactor = 0.05f;
+    [SerializeField] float pitchMovingFactor = 5f;
+    [SerializeField] float yawPositionFactor = 0.2f;
+    [SerializeField] float rollMovingFactor = 10f;
 
     [SerializeField] InputAction movement;
 
     float horiz, verti;
+    float tPitch = 0.5f, tRoll = 0.5f;
 
     void OnEnable() {
         movement.Enable();
@@ -35,11 +36,12 @@ public class PlayerController : MonoBehaviour
 
     void rotate() {
         // Pitch when moving, and based on position
-        float pitch = transform.localPosition.y * -pitchPositionFactor - pitchMovingFactor * verti;
+        float pitch = transform.localPosition.y * pitchPositionFactor - pitchMovingFactor * verti;
         // Yaw based on horizontal position
         float yaw = transform.localPosition.x * -yawPositionFactor;
         // Roll when moving horizontally
         float roll = -rollMovingFactor * horiz;
+
 
         transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
