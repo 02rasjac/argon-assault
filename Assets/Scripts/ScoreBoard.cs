e2a87acf262ca69d916aa2c618b5cfdf43e2df3a
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2022-07-10 12:54:35 +0200

    Lecture #96: Update score on HUD

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour {
 
     int score = 0;
+    TMP_Text scoreText;
+
+    void Start() {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "Start";
+    }
 
     public void IncreaseScore(int increaseAmmount) {
         score += increaseAmmount;
-        Debug.Log("Score: " + score);
+        scoreText.text = score.ToString();
     }
 }
